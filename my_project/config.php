1e9803af9a1a872e166888b64ffb3f3d6f290891
Saurove Dash <royalcse2752@gmail.com>
2017-02-21 05:33:13 -0800

    Update config.php

--- my_project/config.php
+++ my_project/config.php
@@ -3,7 +3,7 @@
 //mysql_connect("localhost","root","") or die("Can not connect to the server");
 //mysql_select_db("data_me") or die("can not connect to database");
 
-
+ 
 $dbhost="localhost";
 $dbname="project";
 $dbuser="root";
